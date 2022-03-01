8f475496c4d6c6d5ff1bee419ed575d782d2a855
zou-justin <justin82049@gmail.com>
2022-03-01 15:35:01 -0500

    work from class

---
+++ Lab01-monocypher/frequency.py
@@ -0,0 +1,10 @@
+def Read(fileName):
+    alphabet = {}
+    with open(fileName,'r') as file:
+        for i in file.read():
+            if i = 'a'
+            
+            
+
+
+Read('temp.txt')
