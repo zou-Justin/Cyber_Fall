da36096386ccfd9a7436d7562cbe23fdfce6f597
Justin <justin82049@gmail.com>
2022-03-01 21:01:26 -0500

    fixed error so it accounts for uppercase

--- Lab01-monocypher/frequency.py
+++ Lab01-monocypher/frequency.py
@@ -2,13 +2,14 @@ import string
 import math
 
 def Read(fileName):
-    alphabet = list(string.ascii_lowercase)
+    alphabet = list(string.ascii_letters)
+    print(alphabet)
     dict = {}
-    for k in range(26):
+    for k in range(52):
         dict[alphabet[k]] = 0
     with open(fileName,'r') as file:
         for i in file.read():
-            for k in range(26): 
+            for k in range(52): 
                 if alphabet[k] == i: 
                     dict[alphabet[k]] += 1;  
     return dict
@@ -17,15 +18,17 @@ def EuclidianDistance(fileName, baseFile):
     distance = 0
     value = list(Read(fileName).values())
     value2 = list(Read(baseFile).values())
-    for i in range(value.count):
-        for j in range (value2.count):
+    for i in range(len(value)):
+        for j in range(len(value2)):
             distance += ((value[i] - value2[j]) ** 2)
     return math.sqrt(distance)
 
+# print(EuclidianDistance("temp.txt","temp2.txt"))
 
 values = Read('temp.txt').values()
+print(values)
 total = sum(values)
 key_list = list(Read('temp.txt').keys())
 val_list = list(values)
-for i in range(26):
+for i in range(52):
     print(key_list[i] + " " + str(val_list[i]/total))
