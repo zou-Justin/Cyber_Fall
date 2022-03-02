3c3e940feb67c62f51082fe5074f0b2dd2191ce8
Justin <justin82049@gmail.com>
2022-03-01 19:03:43 -0500

    work on part 1

--- Lab01-monocypher/frequency.py
+++ Lab01-monocypher/frequency.py
@@ -1,10 +1,21 @@
+import string;
+
 def Read(fileName):
-    alphabet = {}
+    alphabet = list(string.ascii_lowercase)
+    dict = {}
+    counter = 0
+    for k in range(26):
+        dict[alphabet[k]] = 0
     with open(fileName,'r') as file:
         for i in file.read():
-            if i = 'a'
-            
-            
+            for k in range(26): 
+                if alphabet[k] == i: 
+                    dict[alphabet[k]] += 1;  
+    return dict
 
-
-Read('temp.txt')
+values = Read('temp.txt').values()
+total = sum(values)
+key_list = list(Read('temp.txt').keys())
+val_list = list(values)
+for i in range(26):
+    print(key_list[i] + " " + str(val_list[i]/total))
