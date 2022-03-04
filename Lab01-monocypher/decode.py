09c56f849ebb3bf7fe3351ae098a4efe3ac2a813
zou-justin <justin82049@gmail.com>
2022-03-04 15:26:38 -0500

    work

--- Lab01-monocypher/decode.py
+++ Lab01-monocypher/decode.py
@@ -4,12 +4,13 @@ import sys
 
 #reverse alphabet is a different case from simply shifiting down by 1 so account for it
 def decode(Caesar_text = sys.argv[1]):
-    newText = Caesar_text
+    newText = ""
     while True:
         dist = EuclidianDistance(Caesar_text,"temp.txt")
         if (dist <= .019):
-            #return something
-            return "this is good"
+            with open(Caesar_text,'r') as file:
+                newText = file.read()
+            return newText
         else:
             shift(Caesar_text)
 
@@ -17,5 +18,7 @@ def shift(Text):
     for i in Text:
         chr(ord(i)+1)
 
+if __name__ == '__main__':
+    decode()
 
 
