965ea702466d2cc8b994730aefdcda232cff8f6c
zou-justin <justin82049@gmail.com>
2022-03-11 15:36:29 -0500

    udates on encode

--- Lab02-Vigenere/encode.py
+++ Lab02-Vigenere/encode.py
@@ -2,10 +2,23 @@ import sys
 import string
 
 
-def encode():
+def encode(fileName, keyFile):
     Alphabet = string.ascii_uppercase
     dict = {}
+    file1 = open(fileName,'r')
+    file2 = open(keyFile,'r')
+    newText = ""
+    b = ''
     for i in Alphabet:
         dict[i] = ord(i) - 65
+    for i in file1.read():
+        for k in file2.read():
+            print(str(k.upper()))
+            print(dict[str(k.upper())])
+            print(dict["B"])
+    #         newText += chr(ord(i) + dict[k.upper()])
     print(dict)
-encode()
+
+
+if __name__ == '__main__':
+    encode("temp.txt","temp2.txt")
