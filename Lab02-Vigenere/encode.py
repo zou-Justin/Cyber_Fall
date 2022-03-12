ba456ed886382188b02e0c206849a10da833307e
Justin <justin82049@gmail.com>
2022-03-12 17:09:22 -0500

    encode done

--- Lab02-Vigenere/encode.py
+++ Lab02-Vigenere/encode.py
@@ -8,17 +8,19 @@ def encode(fileName, keyFile):
     file1 = open(fileName,'r')
     file2 = open(keyFile,'r')
     newText = ""
-    b = ''
+    file1R = file1.read()
+    file1Read = ""
+    for i in file1R:
+        if (i.isupper() or i.islower()):
+            file1Read += i
+    print(file1Read)
+    file2Read = file2.read()
     for i in Alphabet:
         dict[i] = ord(i) - 65
-    for i in file1.read():
-        for k in file2.read():
-            print(str(k.upper()))
-            print(dict[str(k.upper())])
-            print(dict["B"])
-    #         newText += chr(ord(i) + dict[k.upper()])
-    print(dict)
+    for i in range(len(file1Read)):
+        newText += chr((ord(file1Read[i].upper()) -65 + dict[file2Read[i % len(file2Read)].upper()]) % 26 + 65)
+    return newText
 
 
 if __name__ == '__main__':
-    encode("temp.txt","temp2.txt")
+    print(encode("temp.txt","temp2.txt"))
