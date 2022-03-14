def2d7a62112d72576863a3bc3b90144277dcde0
Justin <justin82049@gmail.com>
2022-03-13 22:04:06 -0400

    work updated to just test the code out

--- Lab02-Vigenere/Crack.py
+++ Lab02-Vigenere/Crack.py
@@ -113,44 +113,52 @@ def guessKeyLength(fileName):
 
 def Decypher(fileName):
     file = open(fileName,'r')
-    Length = 1
+    Length = 5
     fileText = file.read()
     file.close()
     strName = "Pile"
     finalText = ""
     actualFinalText = ""
-    minimumDistance = 1000
+    minimumDistance = 1
     Decode = ""
-    for j in range(16):
-        piles = [""] * Length
-        for i in range(len(fileText)):
-            for k in range(Length):
-                if i % Length == k:
-                    piles[i%Length] += fileText[i]
-        for i in range(Length):
-            Pile1 = open(strName + str(i),'w')
-            Pile1.write(piles[i%Length])
-            Pile1.close()
-            Decode = decode(strName + str(i))
-            for i in range(len(Decode)):
-                finalText += piles[i % Length][i//Length]
-        pile23 = open('newText.txt','w')
-        pile23.write(finalText)
-        pile23.close()
-        dist = EuclidianDistance('newText.txt',"temp.txt")
-        if (dist <= 0.02):
-            # minimumDistance = dist
-            p = open('newText.txt','r')
-            actualFinalText = p.read()
-            p.close()
-            finalText = ""
-            return actualFinalText
-        for i in range(Length):
-            os.remove(strName + str(i))
-        Length+=1
-        os.remove('newText.txt')
+    # for j in range(5):
+    piles = [""] * Length
+    for i in range(len(fileText)):
+        if i % Length == 0:
+                piles[i%Length] += fileText[i]
+        elif i % Length == 1:
+                piles[i%Length] += fileText[i]
+        elif i % Length == 2:
+                piles[i%Length] += fileText[i]
+        elif i % Length == 3:
+                piles[i%Length] += fileText[i]
+        elif i % Length == 4:
+                piles[i%Length] += fileText[i]
+        # elif i % Length == 0:
+        #         piles[i%Length] += fileText[i]
         
-        
+        # for k in range(Length):
+            
+    for i in range(Length):
+        Pile1 = open(strName + str(i),'w')
+        Pile1.write(piles[i])
+        Pile1.close()
+        Decode = decode(strName + str(i))
+    for i in range(len(Decode)):
+        finalText += piles[i % Length][i//Length]
+    pile23 = open('newText.txt','w')
+    pile23.write(finalText)
+    pile23.close()
+    dist = EuclidianDistance('newText.txt',"temp.txt")
+    if (dist <= 0.05):
+        minimumDistance = dist
+        p = open('newText.txt','r')
+        actualFinalText = p.read()
+        p.close()
+    finalText = ""
+    for i in range(Length):
+        os.remove(strName + str(i))
+    #Length+=1
     return actualFinalText
 
 if __name__ == '__main__':
