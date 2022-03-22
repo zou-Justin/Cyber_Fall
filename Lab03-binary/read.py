bfaf3d27a753caa2fd2e3dfa10469dc53e5e2d1d
zou-justin <justin82049@gmail.com>
2022-03-22 15:30:09 -0400

    some progress

--- Lab03-binary/read.py
+++ Lab03-binary/read.py
@@ -1,7 +1,19 @@
-with open('temp.txt','wb') as file:
-    array = [65,66,97,98,10,72,101,108,108,111,32,119,111,114,108,100,10]
-    text = ""
-    for i in array:
-        print(i)
-        a = i.to_bytes(len(array), byteorder='big')
-        file.write(a)
+def read():
+    with open('temp.txt','wb') as file:
+        array = [65,66,97,98,10,72,101,108,108,111,32,119,111,114,108,100,10]
+        for i in array:
+            print(i)
+            a = i.to_bytes(len(array), byteorder='big')
+            file.write(a)
+
+def write():
+    with open('temp.txt','rb') as fileR:
+        for i in fileR.read():
+            print(i)
+            # a = int.from_bytes(i,byteorder='big')
+            # a+=1
+            # c = a.to_bytes(17,byteorder='big')
+            # fileW.write(c)
+
+# read()
+write()
