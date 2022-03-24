80e9310c7d6abb98b6197ba2af87190e468b02b6
Justin <justin82049@gmail.com>
2022-03-24 19:15:53 -0400

    completed task 0.2

--- Lab03-binary/read.py
+++ Lab03-binary/read.py
@@ -2,16 +2,16 @@ def read():
     with open('temp.txt','wb') as file:
         array = [65,66,97,98,10,72,101,108,108,111,32,119,111,114,108,100,10]
         for i in array:
-            a = i.to_bytes(len(array), byteorder='big')
+            a = i.to_bytes(1, byteorder='big')
             file.write(a)
 
 def write():
     with open('temp.txt','rb') as fileR, open("test2.txt", 'wb') as outfile:
         for i in fileR.read():
-            a = int.from_bytes(i,byteorder='big')
-            print(a)
-            c = a.to_bytes(17,byteorder='big')
+            b = i.to_bytes(1,byteorder="big")
+            a = int.from_bytes(b,byteorder='big')
+            a+=1
+            c = a.to_bytes(1,byteorder='big')
             outfile.write(c)
-
 read()
 write()
