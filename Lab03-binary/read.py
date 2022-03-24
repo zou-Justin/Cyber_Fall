1dbee22558711282a9beb2b6b32155c97dc8ee9b
Justin <justin82049@gmail.com>
2022-03-24 18:52:23 -0400

    work for ssh

--- Lab03-binary/read.py
+++ Lab03-binary/read.py
@@ -2,18 +2,16 @@ def read():
     with open('temp.txt','wb') as file:
         array = [65,66,97,98,10,72,101,108,108,111,32,119,111,114,108,100,10]
         for i in array:
-            print(i)
             a = i.to_bytes(len(array), byteorder='big')
             file.write(a)
 
 def write():
-    with open('temp.txt','rb') as fileR:
+    with open('temp.txt','rb') as fileR, open("test2.txt", 'wb') as outfile:
         for i in fileR.read():
-            print(i)
-            # a = int.from_bytes(i,byteorder='big')
-            # a+=1
-            # c = a.to_bytes(17,byteorder='big')
-            # fileW.write(c)
+            a = int.from_bytes(i,byteorder='big')
+            print(a)
+            c = a.to_bytes(17,byteorder='big')
+            outfile.write(c)
 
-# read()
+read()
 write()
