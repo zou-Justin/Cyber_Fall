6a64fb0db765b17dbe26331b1a5cf5d29a048662
Justin <justin82049@gmail.com>
2022-03-24 22:58:11 -0400

    encode perhaps working with xor

---
+++ Lab03-binary/encode.py
@@ -0,0 +1,11 @@
+from hexread import hexFile
+
+def xorEncode(fileName,keyFile):
+    with open(fileName,'rb') as file1,open(keyFile,'rb') as file2,open(keyFile,'wb') as file3:
+        file = file1.read()
+        key = file2.read()
+        for i in range(len(file1.read())):
+            a = (file[i] ^ key[i % len(key.read())])
+        file3.write(a)
+        
+xorEncode("test.txt","test2.txt")
