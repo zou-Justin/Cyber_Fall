3ed5016d4879c7fb7f24364032a59efacb42ba23
Justin <justin82049@gmail.com>
2022-03-24 21:04:23 -0400

    finished it

--- Lab03-binary/hexread.py
+++ Lab03-binary/hexread.py
@@ -1,9 +1,19 @@
+import string
+import sys
+
 def hexFile(fileName):
     hexcode = ""
-    with open('test.txt','r') as file:
+    finalText = ""
+    alphabet = list(string.ascii_lowercase)
+    with open(fileName,'r') as file:
         for i in file.read():
-          hexcode += hex(ord(i)) + " "
-    return hexcode
+            if len(hex(ord(i)).lstrip("0x")) == 1 and hex(ord(i)).lstrip("0x").isalpha():
+                hexcode += "0" + hex(ord(i)).lstrip("0x")
+            else:
+                hexcode += hex(ord(i)).lstrip("0x")
+    finalText = ' '.join(hexcode[i:i+4] for i in range(0,len(hexcode),3))
+    return finalText
 
 
 print(hexFile('test.txt'))
+print(hex(1))
