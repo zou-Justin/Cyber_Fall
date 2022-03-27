fee438266548cfbad63861cb7009a2ab901d63c8
Justin <justin82049@gmail.com>
2022-03-27 19:07:15 -0400

    proper spacing

--- Lab03-binary/hexread.py
+++ Lab03-binary/hexread.py
@@ -6,7 +6,7 @@ def hexFile(fileName):
     finalText = ""
     with open(fileName,'rb') as file:
         hexcode = file.read().hex()
-    finalText = ' '.join(hexcode[i:i+4] for i in range(0,len(hexcode),4))
+    finalText = ' '.join(hexcode[i:i+2] for i in range(0,len(hexcode),2))
     return finalText
 
 if __name__ == '__main__':
