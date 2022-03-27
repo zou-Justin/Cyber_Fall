09597956d8454ff1d26ee4b945dca122e128e820
Justin <justin82049@gmail.com>
2022-03-27 15:36:13 -0400

    everything works except for the end

--- Lab03-binary/decode.py
+++ Lab03-binary/decode.py
@@ -1,9 +1,46 @@
+from cgi import test
+import sys
+from hexread import hexFile
+from binascii import unhexlify
+
 def xorDecode(fileName,keyFile):
-    with open(fileName,'rb') as file1,open(keyFile,'rb') as file2,open(keyFile,'wb') as file3:
-        file = file1.read()
-        key = file2.read()
-        for i in range(len(file1.read())):
-            a = (file[i] ^ key[i % len(key.read())])
-        file3.write(a)
-    
-xorDecode("test2.txt","output.txt")
+    fileHex = hexFile(fileName)
+    keyHex = hexFile(keyFile)
+    print(fileHex)
+    print(keyHex)
+    fileBinary = ""
+    keyBinary = ""
+    finalText = ""
+    for i in fileHex:
+        if i.isalnum():
+            if len(bin(int(i,16)).lstrip("0b")) == 3:
+                fileBinary += "0" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) == 2:
+                fileBinary += "00" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) == 1:
+                fileBinary += "000" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) == 0:
+                fileBinary += "0000" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) >= 4:
+                fileBinary += bin(int(i,16)).lstrip("0b")
+    for i in keyHex:
+        if i.isalnum():
+            if len(bin(int(i,16)).lstrip("0b")) == 3:
+                keyBinary += "0" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) == 2:
+                keyBinary += "00" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) == 1:
+                keyBinary += "000" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) == 0:
+                keyBinary += "0000" + bin(int(i,16)).lstrip("0b")
+            elif len(bin(int(i,16)).lstrip("0b")) >= 4:
+                keyBinary += bin(int(i,16)).lstrip("0b")
+    for i in range(len(fileBinary)):
+       a = int(fileBinary[i],2) ^ int(keyBinary[i % len(keyBinary)],2)
+       finalText += str(a)
+    f = hex(int(finalText,2))
+    f = f.lstrip('0x')
+    return unhexlify(f.rstrip())
+
+if __name__ == '__main__':
+    print(xorDecode(sys.argv[1],sys.argv[2]))
--- Lab03-binary/hexread.py
+++ Lab03-binary/hexread.py
@@ -4,15 +4,16 @@ import sys
 def hexFile(fileName):
     hexcode = ""
     finalText = ""
-    alphabet = list(string.ascii_lowercase)
-    with open(fileName,'r') as file:
+    with open(fileName,'rb') as file:
         for i in file.read():
-            if len(hex(ord(i)).lstrip("0x")) == 1 and hex(ord(i)).lstrip("0x").isalpha():
-                hexcode += "0" + hex(ord(i)).lstrip("0x")
+            if len(hex(i).lstrip("0x")) == 1 and hex(i).lstrip("0x").isalnum():
+                hexcode += "0" + hex(i).lstrip("0x")
             else:
-                hexcode += hex(ord(i)).lstrip("0x")
+                hexcode += hex(i).lstrip("0x")
     finalText = ' '.join(hexcode[i:i+4] for i in range(0,len(hexcode),4))
     return finalText
+
+
 
 if __name__ == '__main__':
     print(hexFile(sys.argv[1]))
